7317d22542980106eb0954ae2172b16f075447d0
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-11-29 17:59:38 +0100

    Add ParkingGrainUser implementation

--- Agenci.Grains/ParkingGrain.cs
+++ Agenci.Grains/ParkingGrain.cs
@@ -7,15 +7,18 @@ public class ParkingGrain : IParkingGrain, IParkingUserGrain
 {
     private readonly IPersistentState<List<Reservation>> _reservations;
     private readonly IPersistentState<ParkingInfo> _parkingInfo;
+    private readonly IGrainFactory _grainFactory;
     
     public ParkingGrain(
         [PersistentState("parkingIntervals", "parkingIntervalsStore")]
         IPersistentState<List<Reservation>> reservations,
         [PersistentState("parkingDto", "parkingDtoStore")]
-        IPersistentState<ParkingInfo> parkingInfo)
+        IPersistentState<ParkingInfo> parkingInfo,
+        IGrainFactory grainFactory)
     {
         _reservations = reservations;
         _parkingInfo = parkingInfo;
+        _grainFactory = grainFactory;
     }
     
     public Task<ParkingOffer> GetParkingPriceResponse(DateTime start, DateTime end, string driverKey)
@@ -30,11 +33,28 @@ public class ParkingGrain : IParkingGrain, IParkingUserGrain
 
     public Task<ParkingDetailedInfo> GetParkingDetailedInfo()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(
+            new ParkingDetailedInfo
+            {
+                Key = _parkingInfo.State.Key,
+                Address = _parkingInfo.State.Address,
+                Latitude = _parkingInfo.State.Latitude,
+                Longitude = _parkingInfo.State.Longitude,
+                MaxCapacity = _parkingInfo.State.MaxCapacity,
+                Reservations = _reservations.State,
+            }
+        );
     }
 
-    public Task UpdateParkingInfo(ParkingInfo parkingInfo)
+    public async Task UpdateParkingInfo(ParkingInfo parkingInfo)
     {
-        throw new NotImplementedException();
+        if (parkingInfo.Key != this.GetPrimaryKeyString())
+        {
+            throw new ArgumentException("ParkingInfo key must be equal to this grain key");
+        }
+        _parkingInfo.State = parkingInfo;
+        await _parkingInfo.WriteStateAsync();
+        var orchestratorGrain = _grainFactory.GetGrain<IOrchestratorGrain>(Guid.Empty);
+        await orchestratorGrain.ReceiveParkingInformation(parkingInfo);
     }
 }
