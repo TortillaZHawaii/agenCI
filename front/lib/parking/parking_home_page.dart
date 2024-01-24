a10083e0a98e471d4ec286a9942d7e8d377123c6
robert18x <bookwolf16@gmail.com>
2024-01-24 03:31:19 +0100

    Added parking history (draft)

--- front/lib/parking/parking_home_page.dart
+++ front/lib/parking/parking_home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class ParkingHomePage extends StatelessWidget {
   const ParkingHomePage({
@@ -14,7 +18,89 @@ class ParkingHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Parking $parkingId"),
       ),
-      body: const Placeholder(),
+      body: _HistoryPageBody(parkingId: parkingId),
     );
+  }
+}
+
+class _HistoryPageBody extends StatefulWidget {
+  const _HistoryPageBody({super.key, required this.parkingId});
+  final String parkingId;
+  static const String apiBaseUrl = String.fromEnvironment("API_BASE_URL");
+
+  @override
+  State<_HistoryPageBody> createState() => __HistoryPageBodyState();
+}
+
+class __HistoryPageBodyState extends State<_HistoryPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: _fetchHistory(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final Map<String, dynamic> history = snapshot.data![0]["reservations"] as Map<String, dynamic>;
+
+        final df = DateFormat("MMM dd HH:mm");
+
+        return ListView.separated(
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: history.length,
+          itemBuilder: (context, index) {
+            final item = history[index];
+
+            final startFormatted = df.format(
+              DateTime.parse(item["start"] as String),
+            );
+            final endFormatted = df.format(
+              DateTime.parse(item["end"] as String),
+            );
+            final driverKey = item["driverKey"] as String;
+            final title = "$driverKey";
+
+            final price = item["price"] as num;
+            final priceFormatted = "${price.toStringAsFixed(2)} \$";
+
+            final subtitle = "From $startFormatted to $endFormatted";
+
+            return ListTile(
+              title: Text(title),
+              subtitle: Text(subtitle),
+              trailing: Text(
+                priceFormatted,
+                style: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 16,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> _fetchHistory() async {
+    final response = await http.get(
+      Uri.parse(
+        "${_HistoryPageBody.apiBaseUrl}parkings/${widget.parkingId}",
+      ),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Failed to fetch history");
+    }
+
+    final json = jsonDecode(response.body) as List<dynamic>;
+    return json.cast<Map<String, dynamic>>();
   }
 }
